3b16a349305104c6487f57cba8c4d30f4287fd2d
William Cheausky <williamchenausky@yahoo.com>
2021-02-15 13:15:33 -0500

    wrote first iquiery

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log("testing questions");
+  });
+}
 
 // Function call to initialize app
 init();
